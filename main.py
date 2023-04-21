152d15e27b166143d82ba16bb57ccb0f70c18f85
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-21 19:37:56 +0200

    add csv conversion method

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from input_params import input_params
+from csv_conversion import *
 
+infile = './resources/test.csv'
 
 def main():
     args = input_params.process_input_params()
+    conversion_main.convert_csv(infile)
     print(args)
 
 
