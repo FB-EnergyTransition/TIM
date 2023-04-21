03486a91b4686dcf03a9d2a802d754a3743b8911
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-21 18:32:29 +0200

    add test test_convert_row()

--- tests/test_csv_transformation.py
+++ tests/test_csv_transformation.py
@@ -1,16 +1,16 @@
 import pytest
-import csv_conversion
+from csv_conversion import readcsv
 def test_reformat_time_1():
     input = '01.01.2018 00:00:00'
     expected_output = '2018-01-01T00:00:00.000Z'
 
-    assert csv_conversion.readcsv.reformattime(input) == expected_output
+    assert readcsv.reformattime(input) == expected_output
 
 def test_reformat_time_2():
     input = '23.04.2023 02:05:44'
     expected_output = '2023-04-23T02:05:44.000Z'
 
-    assert csv_conversion.readcsv.reformattime(input) == expected_output
+    assert readcsv.reformattime(input) == expected_output
 
 @pytest.fixture
 def inputcsv():
@@ -18,13 +18,21 @@ def inputcsv():
 @pytest.fixture()
 def outputcsv():
     return '../resources/test_formatted.csv'
-def test_outputcsv():
-    input = inputcsv()
-    output = outputcsv()
-    # input = '../resources/test.csv'
-    # output = '../resources/test_formatted.csv'
 
-    assert csv_conversion.readcsv.convert_csv(input) == output
+def test_convert_row():
+    row = ['01.01.2018 00:00:00', '01.01.2018 00:15:00', '-6,00', '', '']
+    rowlist = [['', '', 0, '2018-01-01T00:15:00.000Z', '2018-01-01T04:00:00.000Z', '2018-01-01T00:00:00.000Z', '-6.00', 'EUR/kWh', 'Preis EXAA 10:15 Auktion']]
+    # expectedoutput = ',,0,2018-01-01T00:15:00.000Z,2018-01-01T04:00:00.000Z,2018-01-01T00:15:00.000Z,-6.00,EUR/kWh,Preis EXAA 10:15 Auktion'
+
+    assert readcsv.convert_row(0, row) == rowlist
+
+# def test_outputcsv():
+#     # input = inputcsv()
+#     # output = outputcsv()
+#     input = '../resources/test.csv'
+#     output = '../resources/test_formatted.csv'
+#
+#     assert readcsv.convert_csv(input) == output
 
 
 
