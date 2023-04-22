d5b93e5d5052bfa2ba47b2721448e2a77e8bdc30
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-22 20:50:44 +0200

    add new methods

--- csv_conversion/conversion_main.py
+++ csv_conversion/conversion_main.py
@@ -1,10 +1,12 @@
 from csv_conversion import read_write_csv
 from csv_conversion import delete_file
+from csv_conversion import write_header
 
 # infile = '../resources/test.csv'
 def convert_csv(infile):
     outfile = infile.replace('.csv', '_formatted.csv')
     delete_file.checkoutfile(outfile)
+    write_header.write_header(outfile)
     read_write_csv.convert_csv(infile, outfile)
 
 # convert_csv(infile)
