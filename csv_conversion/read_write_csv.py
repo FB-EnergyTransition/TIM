e7d8d256b3e95bb495b2cf257aef19c1ebb2f8f0
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-22 21:34:06 +0200

    add method to read information 'measurement' and 'field' from the first line

--- csv_conversion/read_write_csv.py
+++ csv_conversion/read_write_csv.py
@@ -17,26 +17,54 @@ def get_first_and_last_datetime(filename):
 
 def convert_csv(file, outfile):
     startEndArray = get_first_and_last_datetime(file)
+    measurement_field_list = get_measurement_field(file)
     # read and write lines one by one (no saving in memory)
     with open(file, newline='') as csvfile:
         # scip first line with next
         next(csvfile)
         csvreader = csv.reader(csvfile, delimiter=';')
         for index, row in enumerate(csvreader):
-            rowlist = convert_row(index, row, startEndArray)
+            rowlist = convert_row(index, row, startEndArray, measurement_field_list)
             # rowlist = add_startdate_and_enddate(rowlist, startdate, enddate)
             writedatatocsvfile(rowlist, outfile)
 
-def convert_row(index, row, startEndArray):
+def get_measurement_field(file):
+    with open(file, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=';')
+        for row in csvreader:
+            firstline = row
+            break
+
+    measurement_field = firstline[2]
+
+    # 1) extract field (== unit indicated by [])
+    field = measurement_field[measurement_field.find('[') +
+                              1: measurement_field.find(']')]
+
+    # 2) extract rest of string (== measurement) removing field
+    measurement = measurement_field.replace(field, '').\
+        replace('[', '').\
+        replace(']', '').\
+        rstrip()
+
+    measurement_field_list = []
+    measurement_field_list.extend((field, measurement))
+
+    return measurement_field_list
+
+
+
+def convert_row(index, row, startEndArray, measurement_field_list):
     time = reformat_time.reformattime(row[0])
     value = row[2].replace(",", ".")
     # comments == requirements
     starttime = startEndArray[0]  # method to read only first time
     stoptime = startEndArray[1]  # method to read last time
-    # methods to read this information from the first line:
-    # (skipped above)
-    field = "EUR/kWh"
-    measurement = "Preis EXAA 10:15 Auktion"
+
+    # methods to read measurement and field informationfrom the first line:
+    field = measurement_field_list[0]
+    measurement = measurement_field_list[1]
+
 
     rowlist = [['','', index, starttime, stoptime,
                        time, value, field, measurement]]
