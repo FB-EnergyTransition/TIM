2430454db2be79a86965fd249e67e296a66dfc22
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-30 13:15:57 +0200

    change influx.exe path to repo

--- influx_cli/windows/upload.ps1
+++ influx_cli/windows/upload.ps1
@@ -1,10 +1,12 @@
+$dir = Join-Path -Path $PSScriptRoot -ChildPath "\influx"
+cd $dir
+
 function askforconfigfile {
-    cd 'C:\Program Files\InfluxData\influx\'
     .\influx.exe config list
 }
 
 function askforbuckets {
-    cd 'C:\Program Files\InfluxData\influx\'
+#    cd 'C:\Program Files\InfluxData\influx\'
     .\influx.exe bucket list
 }
 
