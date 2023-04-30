a1de161133504ac4178ae27e5c414681ed2dfad8
KlausFreitag <2110859005@fh-burgenland.at>
2023-04-30 20:20:00 +0200

    add further improvements

--- influx_cli/windows/upload.ps1
+++ influx_cli/windows/upload.ps1
@@ -11,13 +11,26 @@ function askforbuckets($bucket) {
     {return $true}
 }
 
+function deletebucket($bucket) {
+    .\influx.exe bucket delete --name $bucket
+}
+
+function create($bucket) {
+    .\influx.exe bucket create --name $bucket
+    Write-Output $bucket "created"
+}
+
 function createnewbucket($bucket) {
     if(askforbuckets($bucket) -eq $true) {
-        Write-Output 'Bucket already exists!'
+        Write-Output 'Bucket already exists! You want to delete the bucket and create it again (y/n)?'
+        $choice = Read-Host
+        if ($choice -eq 'y') {
+            deletebucket($bucket)
+            create($bucket)
+        }
     }
     else{
-        .\influx.exe bucket create --name $bucket
-        Write-Output $bucket "created"
+        create($bucket)
     }
 }
 
