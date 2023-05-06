a1f1d61a417c6aa659c31d1cfe68175402c9214f
KlausFreitag <2110859005@fh-burgenland.at>
2023-05-06 22:37:08 +0200

    add config file

---
+++ config/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+influxdb = {
+    "url": "http://172.22.108.135:8086",
+    "token": "FyHQmuauHLl07gGqwxR_sToKNmCRJSSvXK2ETDSimF"
+                              "FjfwY0zbLFYEFyT7aC-g9gsy1j2_tpOMDC50JSq804WQ==",
+    "org": "TIM"
+}
