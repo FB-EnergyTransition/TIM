cbe263752b5dd11e1d183af93efa3b986c4c6b4e
KlausFreitag <2110859005@fh-burgenland.at>
2023-05-14 15:16:11 +0200

    add soft coded input (infile)

--- upload/file_handling.py
+++ upload/file_handling.py
@@ -1,9 +1,9 @@
 import os
 
 
-def get_all_converted_csvs():
+def get_all_converted_csvs(infile):
     csvs = []
-    for file in os.listdir('./resources/test_resources'):
+    for file in os.listdir(os.path.dirname(infile)):
         if file.endswith('_formatted.csv'):
             csvs.append(file)
             print(file)
